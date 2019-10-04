a4dc16e112514056d31cb1a7b3d9b9869a0305be
vaporwavy <i@ttv8.me>
2019-10-04 16:03:03 +0900

    test menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,12 +29,12 @@ const Layout = props => {
               <li className="nav-home nav-current" role="menuitem">
                 <Link to={`/`}>Home</Link>
               </li>
-              <li className="nav-about" role="menuitem">
+              {/* <li className="nav-about" role="menuitem">
                 <Link to={`/about`}>About</Link>
               </li>
               <li className="nav-elements" role="menuitem">
                 <Link to={`/elements`}>Elements</Link>
-              </li>
+              </li> */}
             </ul>
           </nav>
           <div className="site-head-center">
