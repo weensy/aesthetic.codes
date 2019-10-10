015f273e3e807f003b791a27e81b738c59f1d3c9
vaporwavy <i@ttv8.me>
2019-10-10 16:00:37 +0900

    test layout

--- src/components/postList.js
+++ src/components/postList.js
@@ -4,19 +4,20 @@ import PostTags from "../components/postTags"
 
 export default props => (
   <article className="post-list">
+    <PostTags tags={props.node.frontmatter.tags} />
     <Link to={props.node.fields.slug} className="post-list-link">
       <div className="post-list-content">
-        <PostTags tags={props.node.frontmatter.tags} />
-        <h2 className="post-list-title">
+        <h2 className="post-card-title">
           {props.node.frontmatter.title || props.node.fields.slug}
         </h2>
         <h3>
-        {props.node.excerpt}
+          {props.node.excerpt}
         </h3>
-        <p>
-        {props.node.frontmatter.date}
-        </p>
+        
       </div>
     </Link>
+    <p>
+          {props.node.frontmatter.date}
+    </p>
   </article>
 )
