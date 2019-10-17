33cc869532331057f66a4f8d60b93186f4f620df
vaporwavy <i@ttv8.me>
2019-10-17 17:12:57 +0900

    test description

--- src/components/postList.js
+++ src/components/postList.js
@@ -7,11 +7,11 @@ export default props => (
     <div className="post-list-content">
       <PostTags tags={props.node.frontmatter.tags} />
       <Link to={props.node.fields.slug} className="post-list-link">
-        <h2 className="post-card-title">
+        <h2 className="post-list-title">
           {props.node.frontmatter.title || props.node.fields.slug}
         </h2>
         <p>
-          {props.node.excerpt}
+          {props.node.frontmatter.description || props.node.excerpt}
         </p>
       </Link>
       <span>
