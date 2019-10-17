6971ac441dbe5f5f95f83cf8ee12b095935aa3af
vaporwavy <i@ttv8.me>
2019-10-17 11:24:13 +0900

    fix postlist

--- src/components/postList.js
+++ src/components/postList.js
@@ -4,16 +4,16 @@ import PostTags from "../components/postTags"
 
 export default props => (
   <article className="post-list">
-    <PostTags tags={props.node.frontmatter.tags} />
     <div className="post-list-content">
+      <PostTags tags={props.node.frontmatter.tags} />
       <Link to={props.node.fields.slug} className="post-list-link">
         <h2 className="post-card-title">
           {props.node.frontmatter.title || props.node.fields.slug}
         </h2>
+        <p>
+          {props.node.excerpt}
+        </p>
       </Link>
-      <p>
-        {props.node.excerpt}
-      </p>
       <span>
         {props.node.frontmatter.date}
       </span>
