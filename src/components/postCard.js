6d22bed973f53a4aa75cba482ff3c0cc236fa6fc
vaporwavy <i@ttv8.me>
2019-10-18 14:41:45 +0900

    test postcard

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -15,7 +15,20 @@ export default props => (
       }
     }
   >
-    <div className=""><Tags tags={props.node.frontmatter.tags}/></div>
+    <div className="post-card-content">
+    <Tags tags={props.node.frontmatter.tags}/>
+    <Link to={props.node.fields.slug} className="post-card-link">
+        <h2 className="post-card-title">
+          {props.node.frontmatter.title || props.node.fields.slug}
+        </h2>
+    </Link>
+    {props.node.frontmatter.description || props.node.excerpt}
+    <Link to={props.node.fields.slug} className="post-card-link">
+      Read more
+    </Link>
+    {props.node.frontmatter.date}
+    </div>
+    {/* <div className=""><Tags tags={props.node.frontmatter.tags}/></div>
     <Link to={props.node.fields.slug} className="post-card-link">
       <div className="post-card-content">
         <h2 className="post-card-title">
@@ -27,6 +40,6 @@ export default props => (
     <Link to={props.node.fields.slug} className="post-card-link">
       <div>Read more</div>
     </Link>
-    <div>{props.node.frontmatter.date}</div>
+    <div>{props.node.frontmatter.date}</div> */}
   </article>
 )
