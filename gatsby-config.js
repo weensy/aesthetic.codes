8d37141158c38ad759ea764083fd3b87d01dc3c1
vaporwavy <i@ttv8.me>
2019-11-04 01:14:26 +0900

    fix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,14 +72,14 @@ module.exports = {
         // develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
         // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-151345403-1`,
       },
     },
     `gatsby-plugin-feed`,
