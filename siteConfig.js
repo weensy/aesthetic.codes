dbc671f587b900971221e96b0f06089898a64ca2
vaporwavy <i@ttv8.me>
2019-11-13 17:03:13 +0900

    Update siteConfig.js
    Add description.

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: 'Vaporwavy',
-  shortName: 'Vaporwavy',
-  description: '',
-  url: 'https://vaporwavy.netlify.com', // Domain of your site without prefix!
-  prefix: '/',
-  author: 'Vaporwavie',
-  twitter: ''
+  name: "Vaporwavy",
+  shortName: "Vaporwavy",
+  description: "A blog, focused on something algorithmic",
+  url: "https://vaporwavy.netlify.com", // Domain of your site without prefix!
+  prefix: "/",
+  author: "Vaporwavie",
+  twitter: "",
 }
