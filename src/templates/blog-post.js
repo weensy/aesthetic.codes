970ff2ca69117a37254c877f4c66825c442883cb
vaporwavy <i@ttv8.me>
2019-11-14 15:02:06 +0900

    Fix blog-post.js
    Add import css files.
    <tables.css> is not rendered on netlify, I test this.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,6 +5,9 @@ import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import "../utils/normalize.css"
+import "../utils/css/screen.css"
+
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
